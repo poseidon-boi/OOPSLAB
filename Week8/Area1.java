2b18b6613411157835353eb5d5666ef5649eea8c
poseidon-boi <63670929+poseidon-boi@users.noreply.github.com>
2024-09-18 10:27:58 +0530

    Added shapes to Area1 and Area2
    Added triangle and square to these classes. This follows the deletion of Area3 and Area4, and merges their code into these programs.

--- Week8/Area1.java
+++ Week8/Area1.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-abstract class hasArea {
+abstract class Shape {
     abstract double area();
 }
 
-class Rectangle extends hasArea {
+class Rectangle extends Shape {
 
     double length;
     double breadth;
@@ -20,7 +20,7 @@ class Rectangle extends hasArea {
     }
 }
 
-class Circle extends hasArea {
+class Circle extends Shape {
 
     double radius;
 
@@ -34,15 +34,52 @@ class Circle extends hasArea {
     }
 }
 
+class Triangle extends Shape {
+
+    double base;
+    double height;
+
+    Triangle(double b, double h) {
+        base = b;
+        height = h;
+    }
+
+    @Override
+    double area() {
+        return 0.5 * base * height;
+    }
+}
+
+class Square extends Shape {
+
+    double length;
+
+    Square(double l) {
+        length = l;
+    }
+
+    @Override
+    double area() {
+        return length * length;
+    }
+}
+
 class Area1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter dimensions of rectangle: ");
-        double l = sc.nextDouble(), b = sc.nextDouble(), r;
+        double l = sc.nextDouble(), br = sc.nextDouble(), r;
         System.out.print("Enter radius of circle: ");
         r = sc.nextDouble();
-        Rectangle rec = new Rectangle(l, b);
+        Rectangle rec = new Rectangle(l, br);
         Circle cir = new Circle(r);
-        System.out.println("Area of rectangle = "+rec.area()+"\nArea of circle = "+cir.area());
+        System.out.print("Enter dimensions of triangle: ");
+        double ba = sc.nextDouble(), h = sc.nextDouble(), side;
+        System.out.print("Enter length of square: ");
+        side = sc.nextDouble();
+        Triangle tri = new Triangle(ba, h);
+        Square sq = new Square(size);
+        System.out.println("Area of rectangle = "+rec.area()+"\nArea of circle = "+cir.area()+
+                           "Area of triangle = "+tri.area()+"\nArea of square = "+sq.area());
     }
 }
--- Week8/Area2.java
+++ Week8/Area2.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-interface hasArea {
+interface Shape {
     double area();
 }
 
-class Rectangle implements hasArea {
+class Rectangle implements Shape {
 
     double length;
     double breadth;
@@ -20,7 +20,7 @@ class Rectangle implements hasArea {
     }
 }
 
-class Circle implements hasArea {
+class Circle implements Shape {
 
     double radius;
 
@@ -34,15 +34,52 @@ class Circle implements hasArea {
     }
 }
 
+class Triangle implements Shape {
+
+    double base;
+    double height;
+
+    Triangle(double b, double h) {
+        base = b;
+        height = h;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
+
+class Square implements Shape {
+
+    double length;
+
+    Square(double l) {
+        length = l;
+    }
+
+    @Override
+    public double area() {
+        return length * length;
+    }
+}
+
 class Area2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter dimensions of rectangle: ");
-        double l = sc.nextDouble(), b = sc.nextDouble(), r;
+        double l = sc.nextDouble(), br = sc.nextDouble(), r;
         System.out.print("Enter radius of circle: ");
         r = sc.nextDouble();
-        Rectangle rec = new Rectangle(l, b);
+        Rectangle rec = new Rectangle(l, br);
         Circle cir = new Circle(r);
-        System.out.println("Area of rectangle = "+rec.area()+"\nArea of circle = "+cir.area());
+        System.out.print("Enter dimensions of triangle: ");
+        double ba = sc.nextDouble(), h = sc.nextDouble(), side;
+        System.out.print("Enter length of square: ");
+        side = sc.nextDouble();
+        Triangle tri = new Triangle(ba, h);
+        Square sq = new Square(size);
+        System.out.println("Area of rectangle = "+rec.area()+"\nArea of circle = "+cir.area()+
+                           "Area of triangle = "+tri.area()+"\nArea of square = "+sq.area());
     }
 }
